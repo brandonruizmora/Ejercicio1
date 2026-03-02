821ca94af37b95e7cb3d283121b2f8731c98d1a8
Tch BRM <utr.tch@utr.edu.mx>
2026-03-02 11:28:08 -0600

    While

---
+++ Ejercicio1/practica1.js
@@ -0,0 +1,12 @@
+// crear un array vacio
+let array = [];
+// crear el contador de los numero 1 al 20
+let contador = 0;
+// crear el while
+
+while(contador < 20) {
+    contador++;
+    array.push(contador)
+}
+// imprimirlos
+console.log(array)
